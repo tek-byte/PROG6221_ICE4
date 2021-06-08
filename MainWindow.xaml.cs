c58417d87a77e3ca13acaca666aa5547bd2e5203
Kenan West <19014225@vcconnect.co.za>
2021-06-08 16:55:00 +0200

    New

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PROG6221_ICE4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string pathA = System.IO.Path.GetFullPath(@"..\..\") + "loginDetails.txt";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StreamReader sr = new StreamReader(pathA, true);
+
+            string[] Usernames = new string[File.ReadAllLines(pathA).Count()/2];
+            string[] Passwords = new string[File.ReadAllLines(pathA).Count()/2];
+
+            for (int x = 0; x < Usernames.Length; x++)
+            {
+                Usernames[x] = sr.ReadLine();
+                Passwords[x] = sr.ReadLine();
+            }
+
+            //Checks if usernames and passwords match with same indexes.
+            if (Usernames.Contains(txtLogin.Text) && Passwords.Contains(txtPassword.Password) && Array.IndexOf(Usernames, txtLogin.Text) == Array.IndexOf(Passwords, txtPassword.Password))
+            {
+                
+                WelcomePage pg = new WelcomePage();
+                pg.lblUserName.Content = txtLogin.Text;
+                this.Hide();
+                //Swithes windows
+                pg.ShowDialog();
+            } 
+            else
+            {
+                lblMain2.Content = "Incorrect!";
+            }
+               
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(pathA))
+                {
+                    using (StreamWriter sw = File.CreateText(pathA))
+                    {
+                        sw.WriteLine(txtLogin.Text);
+                        sw.WriteLine(txtPassword.Password);
+                    }
+                    lblMain2.Content = "Registered!";
+                }
+                else
+                {
+                    using (StreamWriter sw = new StreamWriter(pathA, true))
+                    {
+                        sw.WriteLine(txtLogin.Text);
+                        sw.WriteLine(txtPassword.Password);
+                    }
+                    lblMain2.Content = "Registered!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ WelcomePage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROG6221_ICE4
+{
+    /// <summary>
+    /// Interaction logic for WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Window
+    {
+        MainWindow mw = new MainWindow();
+        private static string pathb = System.IO.Path.GetFullPath(@"..\..\") + "UserDetails.txt";
+      
+        
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+            listView.Items.Clear();
+                if (!File.Exists(pathb))
+                {
+                    using (StreamWriter sw = File.CreateText(pathb))
+                    {
+                    sw.WriteLine("Movies");
+                    sw.WriteLine("Watch new SpiderMan");
+                    sw.WriteLine("7 November 2021");
+                    sw.WriteLine("17:00");
+                    sw.Close();
+                    }
+                    //txtView.Text = "Nothing in here.";
+                }
+
+            try
+            {
+                StreamReader sr = new StreamReader(pathb, true);
+                for (int x = 0; x != File.ReadAllLines(pathb).Count() / 4; x++)
+                {
+                    listView.Items.Add("Plan: " + sr.ReadLine() + "\n");
+                    listView.Items.Add("Description: " + sr.ReadLine() + "\n");
+                    listView.Items.Add("Date: " + sr.ReadLine() + "\n");
+                    listView.Items.Add("Time: " + sr.ReadLine() + "\n");
+                    listView.Items.Add("\n");
+
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured:" + ex);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            listView.Items.Clear();
+            txtplan.Text = String.Empty;
+            txtdesc.Text = String.Empty;
+            txtdate.Text = String.Empty;
+            txtTime.Text = String.Empty;
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            mw.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(pathb))
+                {
+                    using (StreamWriter sw = File.CreateText(pathb))
+                    {
+                        sw.WriteLine("Movies");
+                        sw.WriteLine("Watch new SpiderMan");
+                        sw.WriteLine("7 November 2021");
+                        sw.WriteLine("17:00");
+                        sw.Close();
+                    }
+                    //txtView.Text = "Nothing in here.";
+                }
+                else
+                {
+                    using (StreamWriter sw = new StreamWriter(pathb, true))
+                    {
+                        sw.WriteLine(txtplan.Text);
+                        sw.WriteLine(txtdesc.Text);
+                        sw.WriteLine(txtdate.Text);
+                        sw.WriteLine(txtTime.Text);
+                        sw.Close();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex));
+                throw;
+            }
+            //lblUserName.Content = "Saved!";
+        }
+
+    }
+}
